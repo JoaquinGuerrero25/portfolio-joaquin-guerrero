75926936648ca6c53adc2b0c9a8d1b1e7f5264ad
JoaquinGuerrero25 <joaquinguerrero256@gmail.com>
2025-09-02 00:01:00 -0300

    chore(config): add GitHub Pages deploy setup with vite base path

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { Theme } from "./providers/theme";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ThemeProvider theme={Theme} defaultMode="system" disableTransitionOnChange>
-            <BrowserRouter>
+            <BrowserRouter basename="/portfolio-jg">
                 <CssBaseline />
                 <App />
             </BrowserRouter>
